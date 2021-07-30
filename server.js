4cafb3c553092e4ead1232e78ed04d77678a357f
Tom Hanks <amaresch@gmail.com>
2021-07-30 01:55:57 -0500

    updated server.js

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
+//app.listen(PORT, () => {
+//  console.log(`App running on port ${PORT}!`);
+//});
