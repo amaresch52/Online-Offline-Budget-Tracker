f07e83fba87d1a8915b30aed1c1282078bf1d831
Tom Hanks <amaresch@gmail.com>
2021-07-29 22:55:02 -0500

    updated file format, service worker, and webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./public/index.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: "Budget Tracker App",
+      short_name: "BTApp",
+      description: "Budget tracking",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-192x192.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("public", "icons"),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
